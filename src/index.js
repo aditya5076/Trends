033caf458b344a759d062cc7de6cc9cf6458d88c
aditya5076 <adinarvekar8454@gmail.com>
2021-03-11 23:36:35 +0530

    added redux-persist and configured store and root-reducer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
     </Router>
   </Provider>,
   document.getElementById("root")
